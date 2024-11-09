173fd44e7f5fd7948c58f42a3b7d9a541c3eeba6
Sergera60 <112910302+Sergera60@users.noreply.github.com>
2024-11-09 21:51:41 +0100

    test

---
+++ TestInfrastructure/TestContext.cs
@@ -0,0 +1,28 @@
+﻿using AM.ApplicationCore.Domain;
+using AM.Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Infrastructure
+{
+    public class TestContext : DbContext
+    {
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb; 
+                                        Initial Catalog=AirportManagementDB;
+                                        Integrated Security=true ;
+                                        MultipleActiveResultSets=True");
+              
+            base.OnConfiguring(optionsBuilder);
+        }
+       
+    }
+}
